c8badccfb643007a6547c446d4c65f614cfca643
Jack Sweeney <jackcsweeney@gmail.com>
2024-08-08 14:49:00 -0700

    feat: Add definition for user model

---
+++ app/lib/definitions.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  location: string;
+  search_radius: number;
+  created_at: string;
+  updated_at: string;
+};
